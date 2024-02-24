a3b109cba2c5a7d85b49e82937b4b6a161a12c43
Sonja Selvijovska <sonja.selvijovska@emitknowledge.com>
2024-02-24 16:19:17 +0100

    itemDescriptionCard component changes

--- src/components/ItemDescriptionCard.jsx
+++ src/components/ItemDescriptionCard.jsx
@@ -1,149 +1,159 @@
-import classes from "../pages/shoppage/ShopPage.module.css";
+// import classes from "../pages/shoppage/ShopPage.module.css";
+import ArmorDescription from "./ArmorDescription";
+import PotionDescription from "./PotionDescription";
+import WeaponDescription from "./WeaponDescription";
+import GeneralItemDescription from "./GeneralItemDescription";
+
 const ItemDescriptionCard = ({ item }) => {
   let content;
 
-  const propertyList = [];
-  let specialList = [];
-
-  if (item.properties) {
-    for (const property of item.properties) {
-      propertyList.push(property["index"]);
-    }
-  }
-  if (item.special) {
-    for (const special of item.special) {
-      specialList.push(special["index"]);
-    }
-  }
-
-  if (item["equipment_category"].index === "armor") {
-    content = (
-      <div className="item_description">
-        <h4>{item.name}</h4>
-        <p className={classes.itemspecs}> {item["armor_category"]}</p>
-        <p>
-          <span className={classes.categoryDesc}>AC: </span>{" "}
-          {item["armor_class"].base}
-        </p>
-
-        {item.properties.length !== 0 && (
-          <p>
-            <span className={classes.categoryDesc}>Properties: </span>{" "}
-            {propertyList.join(", ")}
-          </p>
-        )}
-        {item.special.length !== 0 && (
-          <p>
-            <span className={classes.categoryDesc}>Special: </span>{" "}
-            {specialList.join(", ")}
-          </p>
-        )}
-        <p>
-          <span className={classes.categoryDesc}>Weight: </span> {item.weight}
-        </p>
-        <p>
-          <span className={classes.categoryDesc}>Price: </span>{" "}
-          {item.cost.quantity}
-          {item.cost.unit}
-        </p>
-      </div>
-    );
-  }
-  if (item["equipment_category"].index === "potion") {
-    content = (
-      <div className={classes.itemDescription}>
-        <h3>{item.name}</h3>
-        {item.desc.length !== 0 && (
-          <p className={classes.itemspecs}>{item.desc[0]}</p>
-        )}
-        {item.desc.length !== 0 && (
-          <p className={classes.description}>{item.desc[1]}</p>
-        )}
-        {/* {item.properties.length !== 0 && (
-          <p>Properties: {propertyList.join(", ")}</p>
-        )}
-        {item.special.length !== 0 && <p>Special: {specialList.join(", ")}</p>} */}
-        <p>
-          <span className={classes.categoryDesc}>Price:</span>{" "}
-          {item.cost?.quantity || item.rarity.name === "Common" ? "50" : "100"}
-          {item.cost?.unit || "gp"}
-        </p>
-      </div>
-    );
-  } else if (item["equipment_category"].index === "weapon") {
-    content = (
-      <div className={classes.itemDescription}>
-        <h3>{item.name}</h3>
-        {item.properties.length !== 0 && (
-          <p className={classes.itemspecs}>{propertyList.join(", ")}</p>
-        )}
-        <p>
-          <span className={classes.categoryDesc}>Category: </span>
-          {item["weapon_category"]}
-        </p>
-        <p>
-          <span className={classes.categoryDesc}>Range: </span>{" "}
-          {item["weapon_range"]}
-        </p>
-        <p>
-          <span className={classes.categoryDesc}>Damage: </span>{" "}
-          {item.damage["damage_dice"]}, {item.damage["damage_type"].index}
-        </p>
-
-        {item.special.length !== 0 && (
-          <p>
-            <span className={classes.categoryDesc}>Special: </span>{" "}
-            {specialList.join(", ")}
-          </p>
-        )}
-
-        <p>
-          <span className={classes.categoryDesc}>Weight: </span> {item.weight}
-        </p>
-        <p>
-          <span className={classes.categoryDesc}>Price: </span>{" "}
-          {item.cost.quantity}
-          {item.cost.unit}
-        </p>
-      </div>
-    );
-  } else {
-    content = (
-      <div className={classes.itemDescription}>
-        <h3>{item.name}</h3>
-        {item.properties.length !== 0 && (
-          <p>
-            <span className={classes.categoryDesc}>Properties: </span>{" "}
-            {propertyList.join(", ")}
-          </p>
-        )}
-
-        {item.special.length !== 0 && (
-          <p>
-            <span className={classes.categoryDesc}>Special: </span>{" "}
-            {specialList.join(", ")}
-          </p>
-        )}
-        {item.desc.length !== 0 && (
-          <p className={classes.description}>{item.desc[0]}</p>
-        )}
-        <p>
-          <span className={classes.categoryDesc}>Weight: </span> {item.weight}
-        </p>
-
-        <p>
-          <span className={classes.categoryDesc}>Price: </span>{" "}
-          {item.cost.quantity}
-          {item.cost.unit}
-        </p>
-      </div>
-    );
+  // Check the type of item and render the appropriate component
+  switch (item.equipment_category.index) {
+    case "armor":
+      content = <ArmorDescription item={item} />;
+      break;
+    case "potion":
+      content = <PotionDescription item={item} />;
+      break;
+    case "weapon":
+      content = <WeaponDescription item={item} />;
+      break;
+    default:
+      content = <GeneralItemDescription item={item} />;
   }
 
   return content;
 };
 
 export default ItemDescriptionCard;
+
+
+// if (item["equipment_category"].index === "armor") {
+//   content = (
+//     <div className="item_description">
+//       <h4>{item.name}</h4>
+//       <p className={classes.itemspecs}> {item["armor_category"]}</p>
+//       <p>
+//         <span className={classes.categoryDesc}>AC: </span>{" "}
+//         {item["armor_class"].base}
+//       </p>
+
+//       {item.properties.length !== 0 && (
+//         <p>
+//           <span className={classes.categoryDesc}>Properties: </span>{" "}
+//           {propertyList.join(", ")}
+//         </p>
+//       )}
+//       {item.special.length !== 0 && (
+//         <p>
+//           <span className={classes.categoryDesc}>Special: </span>{" "}
+//           {specialList.join(", ")}
+//         </p>
+//       )}
+//       <p>
+//         <span className={classes.categoryDesc}>Weight: </span> {item.weight}
+//       </p>
+//       <p>
+//         <span className={classes.categoryDesc}>Price: </span>{" "}
+//         {item.cost.quantity}
+//         {item.cost.unit}
+//       </p>
+//     </div>
+//   );
+// }
+// if (item["equipment_category"].index === "potion") {
+//   content = (
+//     <div className={classes.itemDescription}>
+//       <h3>{item.name}</h3>
+//       {item.desc.length !== 0 && (
+//         <p className={classes.itemspecs}>{item.desc[0]}</p>
+//       )}
+//       {item.desc.length !== 0 && (
+//         <p className={classes.description}>{item.desc[1]}</p>
+//       )}
+//       {/* {item.properties.length !== 0 && (
+//         <p>Properties: {propertyList.join(", ")}</p>
+//       )}
+//       {item.special.length !== 0 && <p>Special: {specialList.join(", ")}</p>} */}
+//       <p>
+//         <span className={classes.categoryDesc}>Price:</span>{" "}
+//         {item.cost?.quantity || item.rarity.name === "Common" ? "50" : "100"}
+//         {item.cost?.unit || "gp"}
+//       </p>
+//     </div>
+//   );
+// } 
+
+// else if (item["equipment_category"].index === "weapon") {
+//   content = (
+//     <div className={classes.itemDescription}>
+//       <h3>{item.name}</h3>
+//       {item.properties.length !== 0 && (
+//         <p className={classes.itemspecs}>{propertyList.join(", ")}</p>
+//       )}
+//       <p>
+//         <span className={classes.categoryDesc}>Category: </span>
+//         {item["weapon_category"]}
+//       </p>
+//       <p>
+//         <span className={classes.categoryDesc}>Range: </span>{" "}
+//         {item["weapon_range"]}
+//       </p>
+//       <p>
+//         <span className={classes.categoryDesc}>Damage: </span>{" "}
+//         {item.damage["damage_dice"]}, {item.damage["damage_type"].index}
+//       </p>
+
+//       {item.special.length !== 0 && (
+//         <p>
+//           <span className={classes.categoryDesc}>Special: </span>{" "}
+//           {specialList.join(", ")}
+//         </p>
+//       )}
+
+//       <p>
+//         <span className={classes.categoryDesc}>Weight: </span> {item.weight}
+//       </p>
+//       <p>
+//         <span className={classes.categoryDesc}>Price: </span>{" "}
+//         {item.cost.quantity}
+//         {item.cost.unit}
+//       </p>
+//     </div>
+//   );
+// } else {
+//   content = (
+//     <div className={classes.itemDescription}>
+//       <h3>{item.name}</h3>
+//       {item.properties.length !== 0 && (
+//         <p>
+//           <span className={classes.categoryDesc}>Properties: </span>{" "}
+//           {propertyList.join(", ")}
+//         </p>
+//       )}
+
+//       {item.special.length !== 0 && (
+//         <p>
+//           <span className={classes.categoryDesc}>Special: </span>{" "}
+//           {specialList.join(", ")}
+//         </p>
+//       )}
+//       {item.desc.length !== 0 && (
+//         <p className={classes.description}>{item.desc[0]}</p>
+//       )}
+//       <p>
+//         <span className={classes.categoryDesc}>Weight: </span> {item.weight}
+//       </p>
+
+//       <p>
+//         <span className={classes.categoryDesc}>Price: </span>{" "}
+//         {item.cost.quantity}
+//         {item.cost.unit}
+//       </p>
+//     </div>
+//   );
+// }
+
 
 /* 
 EQUIPMENT CATEGORIES:
