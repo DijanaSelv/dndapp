52f649b6c2a54d67534069df00b42343e2ecaec4
DijanaSelv <d.selvijovska@gmail.com>
2024-02-28 20:29:08 +0100

    design and info item card fixed

--- src/components/WeaponDescription.jsx
+++ src/components/WeaponDescription.jsx
@@ -1,33 +1,49 @@
 import classes from "../pages/shoppage/ShopPage.module.css";
 
 const WeaponDescription = ({ item }) => {
-  const { name, properties, weapon_category, weapon_range, damage, special, weight, cost } = item;
+  const {
+    name,
+    properties,
+    weapon_category,
+    weapon_range,
+    damage,
+    special,
+    weight,
+    cost,
+  } = item;
+  console.log(properties);
+  const propertyList = item.properties.map((property) => property.index);
+  const specialList = item.special.map((special) => special.index);
 
   return (
     <div className={classes.itemDescription}>
       <h3>{name}</h3>
       {properties.length > 0 && (
-        <p className={classes.itemspecs}>{properties.join(", ")}</p>
+        <p className={classes.itemspecs}>{propertyList.join(", ")}</p>
       )}
       <p>
-        <span className={classes.categoryDesc}>Category:</span> {weapon_category}
+        <span className={classes.categoryDesc}>Category:</span>{" "}
+        {weapon_category}
       </p>
       <p>
         <span className={classes.categoryDesc}>Range:</span> {weapon_range}
       </p>
       <p>
-        <span className={classes.categoryDesc}>Damage:</span> {damage.damage_dice}, {damage.damage_type.index}
+        <span className={classes.categoryDesc}>Damage:</span>{" "}
+        {damage.damage_dice}, {damage.damage_type.index}
       </p>
       {special.length > 0 && (
         <p>
-          <span className={classes.categoryDesc}>Special:</span> {special.join(", ")}
+          <span className={classes.categoryDesc}>Special:</span>{" "}
+          {specialList.join(", ")}
         </p>
       )}
       <p>
         <span className={classes.categoryDesc}>Weight:</span> {weight}
       </p>
       <p>
-        <span className={classes.categoryDesc}>Price:</span> {cost.quantity}{cost.unit}
+        <span className={classes.categoryDesc}>Price:</span> {cost.quantity}
+        {cost.unit}
       </p>
     </div>
   );
