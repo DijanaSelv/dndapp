3bd92dfb8178881c5558abac6639dd1f560e508c
DijanaSelv <d.selvijovska@gmail.com>
2024-03-17 15:44:01 +0100

    design

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -60,85 +60,92 @@ const MainNav = () => {
   ];
 
   return (
-    <div className={classes.background}>
-      <div className={classes.mainNav}>
-        <div className={classes.welcomeLogo}>
-          <Link className={classes.titleLink}>
-            <div className={classes.appLogo}>
-              <FontAwesomeIcon icon={faDiceD20} className={classes.icon} />
-              <h1 className={classes.title}> Di & Di </h1>
-            </div>
-            <p className={classes.subtitle}>A web app for your RPG campaigns</p>
-          </Link>
-        </div>
-
-        <div className={classes.secondRow}>
-          {uid && (
-            <div className={classes.navLinks}>
-              <NavLink to="/" className={classes.navLink}>
-                Home
-              </NavLink>
-              <NavLink to="/" className={classes.navLink}>
-                Announcements
-              </NavLink>
-            </div>
-          )}
-
-          {firstName && (
-            <Dropdown
-              menu={{ items }}
-              trigger={["click"]}
-              className={classes.navLink}
-            >
-              <div>
-                Hi, {firstName}! <DownOutlined style={{ color: "#3a9fd6" }} />{" "}
-              </div>
-            </Dropdown>
-          )}
-        </div>
-      </div>
-      {params.campaignId && (
-        <div className={classes.campaignNavBackground}>
+    <>
+      {uid && (
+        <div className={classes.background}>
           <div className={classes.mainNav}>
-            <div className={classes.navLinks}>
-              <NavLink
-                to={`/Campaigns/${params.type}/${campaignId}/info`}
-                className={`${classes.navLink} ${classes.campaignNavLink}`}
-              >
-                Info
-              </NavLink>
-              <NavLink
-                to={`/Campaigns/${params.type}/${campaignId}/play`}
-                className={`${classes.navLink} ${classes.campaignNavLink}`}
-              >
-                Play
-              </NavLink>
-              <NavLink
-                to={`/Campaigns/${params.type}/${campaignId}/play/shops`}
-                className={`${classes.navLink} ${classes.campaignNavLink}`}
-              >
-                Shops
-              </NavLink>
-              {/*               <NavLink
-                className={`${classes.navLink} ${classes.campaignNavLink}`}
-              >
-                Combat
-              </NavLink>
-              <NavLink
-                className={`${classes.navLink} ${classes.campaignNavLink}`}
-              >
-                Log
-              </NavLink>
-              <NavLink
-                className={`${classes.navLink} ${classes.campaignNavLink}`}
-              >
-                Notes
-              </NavLink> */}
+            <div className={classes.welcomeLogo}>
+              <Link className={classes.titleLink}>
+                <div className={classes.appLogo}>
+                  <FontAwesomeIcon icon={faDiceD20} className={classes.icon} />
+                  <h1 className={classes.title}> Di & Di </h1>
+                </div>
+                <p className={classes.subtitle}>
+                  A web app for your RPG campaigns
+                </p>
+              </Link>
+            </div>
+
+            <div className={classes.secondRow}>
+              {uid && (
+                <div className={classes.navLinks}>
+                  <NavLink to="/" className={classes.navLink}>
+                    Home
+                  </NavLink>
+                  <NavLink to="/" className={classes.navLink}>
+                    Announcements
+                  </NavLink>
+                </div>
+              )}
+
+              {firstName && (
+                <Dropdown
+                  menu={{ items }}
+                  trigger={["click"]}
+                  className={classes.navLink}
+                >
+                  <div>
+                    Hi, {firstName}!{" "}
+                    <DownOutlined style={{ color: "#3a9fd6" }} />{" "}
+                  </div>
+                </Dropdown>
+              )}
             </div>
           </div>
+          {params.campaignId && (
+            <div className={classes.campaignNavBackground}>
+              <div className={classes.mainNav}>
+                <div className={classes.navLinks}>
+                  <NavLink
+                    to={`/Campaigns/${params.type}/${campaignId}/info`}
+                    className={`${classes.navLink} ${classes.campaignNavLink}`}
+                  >
+                    Info
+                  </NavLink>
+                  <NavLink
+                    to={`/Campaigns/${params.type}/${campaignId}/play`}
+                    className={`${classes.navLink} ${classes.campaignNavLink}`}
+                  >
+                    Play
+                  </NavLink>
+                  <NavLink
+                    to={`/Campaigns/${params.type}/${campaignId}/play/shops`}
+                    className={`${classes.navLink} ${classes.campaignNavLink}`}
+                  >
+                    Shops
+                  </NavLink>
+                  {/*               <NavLink
+            className={`${classes.navLink} ${classes.campaignNavLink}`}
+          >
+            Combat
+          </NavLink>
+          <NavLink
+            className={`${classes.navLink} ${classes.campaignNavLink}`}
+          >
+            Log
+          </NavLink>
+          <NavLink
+            className={`${classes.navLink} ${classes.campaignNavLink}`}
+          >
+            Notes
+          </NavLink> */}
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       )}
-    </div>
+    </>
   );
 };
 
