2789d015c94664ad0de4a762084dfb8121dbbb72
DijanaSelv <d.selvijovska@gmail.com>
2024-06-08 13:33:32 +0200

    character info design. fixed grid layoud added abilities and tooltip for each

--- src/pages/characterpage/CharacterPage.jsx
+++ src/pages/characterpage/CharacterPage.jsx
@@ -1,11 +1,18 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faShield,
+  faHeartPulse,
   faHeart,
   faCoins,
   faEye,
   faPersonRunning,
+  faHandFist,
+  faBullseye,
+  faBrain,
+  faLightbulb,
+  faMasksTheater,
 } from "@fortawesome/free-solid-svg-icons";
+import { Tooltip } from "antd";
 import classes from "./CharacterPage.module.css";
 
 const CharacterPage = () => {
@@ -14,7 +21,7 @@ const CharacterPage = () => {
       <div className={classes.charInfoSection}>
         <h2 className={classes.sectionTitle}>
           {" "}
-          Character Name <span>Race and class</span>
+          Slafko Janevski <span>Human Bard</span>
         </h2>
         <div className={classes.charInfoContent}>
           <img
@@ -42,8 +49,10 @@ const CharacterPage = () => {
                   <FontAwesomeIcon icon={faCoins} />
                   Gold:
                 </span>
-                <span className={classes.statValue}>200gp</span>
+                <span className={classes.statValue}>200gp, 56sp, 5cp</span>
               </div>
+            </div>
+            <div className={classes.statRow}>
               <div className={classes.singleStat}>
                 <span className={classes.statLabel}>
                   <FontAwesomeIcon icon={faPersonRunning} />
@@ -59,37 +68,153 @@ const CharacterPage = () => {
                 <span className={classes.statValue}>12</span>
               </div>
             </div>
-            {/* <div className={classes.statRow}>
-              <div className={classes.singleStat}>
-                <span className={classes.statLabel}>
-                  <FontAwesomeIcon icon={faPersonRunning} />
-                  Initiative:
-                </span>
-                <span className={classes.statValue}>+4</span>
-              </div>
-              <div className={classes.singleStat}>
-                <span className={classes.statLabel}>
-                  <FontAwesomeIcon icon={faEye} />
-                  Passive Perception:
-                </span>
-                <span className={classes.statValue}>12</span>
-              </div>
-            </div> */}
           </div>
         </div>
       </div>
       <div className={classes.charInfoSection}>
         <h2 className={classes.sectionTitle}>
-          Stats <span>Proficiency bonus: +4</span>
+          Ability Scores <span>Proficiency bonus: +4</span>
         </h2>
 
-        <div>Strength</div>
-        <div></div>
-        <div>Dexterity</div>
-        <div>Constitution</div>
-        <div>Intelligence</div>
-        <div>Wisdom</div>
-        <div>Charisma</div>
+        <div className={classes.abilities}>
+          <div className={classes.singleAbility}>
+            <Tooltip title="athletics" trigger={"click"}>
+              <p className={classes.abilityTitle}>
+                {" "}
+                <FontAwesomeIcon icon={faHandFist} />
+                Strength
+              </p>
+            </Tooltip>
+            <div className={classes.abilityCircle}>
+              {" "}
+              <div className={classes.abilityScore}>10</div>
+              <div className={classes.modifierScore}>+0</div>
+            </div>
+          </div>
+          <div className={classes.singleAbility}>
+            <Tooltip
+              title={
+                <>
+                  acrobatics
+                  <br />
+                  sleight of hand
+                  <br />
+                  stealth
+                </>
+              }
+              trigger={"click"}
+            >
+              <p className={classes.abilityTitle}>
+                {" "}
+                <FontAwesomeIcon icon={faBullseye} /> Dexterity
+              </p>
+            </Tooltip>
+            <div className={classes.abilityCircle}>
+              {" "}
+              <div className={classes.abilityScore}>16</div>
+              <div className={classes.modifierScore}>+3</div>
+            </div>
+          </div>
+          <div className={classes.singleAbility}>
+            <Tooltip title={<>endurance</>} trigger={"click"}>
+              <p className={classes.abilityTitle}>
+                {" "}
+                <FontAwesomeIcon icon={faHeartPulse} />
+                Constitution
+              </p>
+            </Tooltip>
+            <div className={classes.abilityCircle}>
+              {" "}
+              <div className={classes.abilityScore}>15</div>
+              <div className={classes.modifierScore}>+3</div>
+            </div>
+          </div>
+          <div className={classes.singleAbility}>
+            <Tooltip
+              title={
+                <>
+                  arcana
+                  <br />
+                  history
+                  <br />
+                  investigation
+                  <br />
+                  nature
+                  <br />
+                  religion
+                </>
+              }
+              trigger={"click"}
+            >
+              <p className={classes.abilityTitle}>
+                {" "}
+                <FontAwesomeIcon icon={faBrain} />
+                Intelligence
+              </p>
+            </Tooltip>
+            <div className={classes.abilityCircle}>
+              {" "}
+              <div className={classes.abilityScore}>9</div>
+              <div className={classes.modifierScore}>-1</div>
+            </div>
+          </div>
+          <div className={classes.singleAbility}>
+            <Tooltip
+              title={
+                <>
+                  animal handling
+                  <br />
+                  insight
+                  <br />
+                  medicine
+                  <br />
+                  perception
+                  <br />
+                  survival
+                </>
+              }
+              trigger={"click"}
+            >
+              <p className={classes.abilityTitle}>
+                {" "}
+                <FontAwesomeIcon icon={faLightbulb} />
+                Wisodm
+              </p>
+            </Tooltip>
+            <div className={classes.abilityCircle}>
+              {" "}
+              <div className={classes.abilityScore}>20</div>
+              <div className={classes.modifierScore}>+5</div>
+            </div>
+          </div>
+          <div className={classes.singleAbility}>
+            <Tooltip
+              title={
+                <>
+                  deception
+                  <br />
+                  intimidation
+                  <br />
+                  performance
+                  <br />
+                  persuasion
+                </>
+              }
+              trigger={"click"}
+            >
+              <p className={classes.abilityTitle}>
+                {" "}
+                <FontAwesomeIcon icon={faMasksTheater} />
+                Charisma
+              </p>
+            </Tooltip>
+            <div className={classes.abilityCircle}>
+              {" "}
+              <div className={classes.abilityScore}>11</div>
+              <div className={classes.modifierScore}>+0</div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
