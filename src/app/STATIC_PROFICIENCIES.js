c6aa2bddfeee5025f6aaa32d727742570374d20a
DijanaSelv <d.selvijovska@gmail.com>
2024-07-02 17:11:38 +0200

    new character page design

---
+++ src/app/STATIC_PROFICIENCIES.js
@@ -0,0 +1,8 @@
+export const PROFICIENCIES = [
+  {
+    key: "armor",
+    label: "",
+  },
+];
+
+/* the problem is language has a different location in the api, i will still need to add that manually */
